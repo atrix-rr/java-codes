09202c3d2933a3de342bbf1ec825d2574bb0676e
Anant Tiwari <ananttiwari231@gmail.com>
2025-10-24 19:44:00 +0530

    updated

--- evenodd.java
+++ evenodd.java
@@ -4,7 +4,7 @@ class evenodd
     public static void main(String args[])
     {
         Scanner scanner=new Scanner(System.in);
-        System.out.println("ENTER A NO.");
+        System.out.println("ENTER A NUMBER.");
         int num=scanner.nextInt();
 
         if(num%2==0)
